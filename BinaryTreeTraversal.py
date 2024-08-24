c16f26d98a7953e85ed812eb844ce54c39230d07
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-08-23 22:20:46 -0700

    Update BinaryTreeTraversal.py

--- BinaryTreeTraversal.py
+++ BinaryTreeTraversal.py
@@ -1,1 +1,56 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
+def in_order_dfs(node):
+    if node is None:
+        return
+    in_order_dfs(node.left)
+    print(node.data, end=' ')
+    in_order_dfs(node.right)
+
+def pre_order_dfs(node):
+    if node is None:
+        return
+    print(node.data, end=' ')
+    pre_order_dfs(node.left)
+    pre_order_dfs(node.right)
+
+def post_order_dfs(node):
+    if node is None:
+        return
+    post_order_dfs(node.left)
+    post_order_dfs(node.right)
+    print(node.data, end=' ')
+
+
+def bfs(root):
+    if root is None:
+        return
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        print(node.data, end=' ')
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+
+if __name__ == "__main__":
+    # Creating the tree
+    root = Node(2)
+    root.left = Node(3)
+    root.right = Node(4)
+    root.left.left = Node(5)
+
+    print("In-order DFS: ", end='')
+    in_order_dfs(root)
+    print("\nPre-order DFS: ", end='')
+    pre_order_dfs(root)
+    print("\nPost-order DFS: ", end='')
+    post_order_dfs(root)
+    print("\nLevel order: ", end='')
+    bfs(root)
